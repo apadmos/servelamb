744405728649815094e02fb87b8128bff33c95d4
Alex <apadmos@>
2026-02-10 19:44:58 -0500

    client facing portion is working

--- resp_builder.py
+++ resp_builder.py
@@ -6,7 +6,7 @@ import sys
 import urllib.parse
 import uuid
 
-from jinja2 import Environment, select_autoescape, FunctionLoader, StrictUndefined
+from jinja2 import Environment, select_autoescape, FunctionLoader
 
 from .jinja_helpers import words_split, date_format, date_input_format
 
@@ -92,8 +92,7 @@ class RespBuilder(object):
             """May need file system loader here https://jinja.palletsprojects.com/en/3.0.x/api/#jinja2.PackageLoader"""
             self._jinja = Environment(
                 loader=FunctionLoader(self.load_template),
-                autoescape=select_autoescape(),
-                undefined=StrictUndefined)
+                autoescape=select_autoescape())
             self._jinja.filters["date"] = date_format
             self._jinja.filters["input_date"] = date_input_format
             self._jinja.filters["words"] = words_split
